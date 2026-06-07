11a3a0a3b5079c7e01bf20090fc5d97b5603b37f
Nixon Abuku <nixonmatara91@gmail.com>
2026-06-07 13:34:45 -0400

    add POST /jobs route - saves rew job to postgres database and returns created job

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/jobs', async function(req, res){
     const result = await pool.query('SELECT * FROM jobs;');
     res.json(result.rows);
 });
+
+app.post('/jobs', async function(req, res){
+    const company = req.body.company
+    const position = req.body.position
+    const status = req.body.status
+    const result = await pool.query('INSERT INTO jobs (company, position, status) VALUES($1, $2, $3) RETURNING *', [company, position, status]);
+    res.status(201).json(result.rows);
+});
+
+
 app.listen(3000, function(){
     console.log("Server started")
 });
